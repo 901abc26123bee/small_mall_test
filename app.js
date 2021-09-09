877c13117db3d072940702e2e39d90fc16b624ec
Jhenyuan Wong <tina168wong@gmail.com>
2021-09-09 12:18:02 +0800

    connect todb + get all data

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+var nanoid = require('nanoid')
 
 
 
@@ -25,10 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.urlencoded({ extended: true}))
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
 
-
+// ---------------------------- page router ------------------------------
 // 頁面轉跳
 app.use('/', indexRouter);
 app.use('/buyer', buyerRouter);
@@ -36,9 +37,115 @@ app.use('/seller', sellerRouter);
 app.use('/cart', cartRouter);
 app.use('/login', loginRouter);
 
+// ---------------------------- database ------------------------------
+// connect to database
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+db.on('error', (err) => console.error('connection error', err)); // 連線異常
+db.once('open', (db) => console.log('Connected to MongoDB')); // 連線成功
+
+const itemsSchema = mongoose.Schema({
+  _id: {
+    type: String,
+    default: () => nanoid()
+  },
+  name: String,
+  price: Number,
+  amount: Number
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const skechers = new Item({
+  name: "skechers",
+  number: 6,
+  price: 3000,
+});
+
+const nike = new Item({
+  name: "nike",
+  number: 5,
+  price: 4000,
+});
+
+const defaultItems = [skechers, nike];
+
+const ListSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const ItemList = mongoose.model("ItemList", ListSchema);
 
 
 
+// ---------------------------- seller/crud ------------------------------
+
+
+const getItemListFromDB = function(req, res) {
+  Item.find({}, (err, foundItems) => {
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, (err) => {
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log("succesfully done! [1]");
+        }
+      })
+    }
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send(foundItems);
+    }
+  });
+}
+
+const deleteItemInDB = (req, res) => {
+  let itemID = req.body.checkBox;
+  const listName = req.body.listName;
+  console.log(listName);
+  if(listName === date.getDate()){
+    Item.findByIdAndDelete(itemID, (err)=>{
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log("succsesfuly deleted item");
+        res.redirect("/");
+      }
+    });
+  }
+  else{
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: itemID} } }, (err, foundList)=>{
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.redirect("/" + listName);
+      }
+    });
+  }
+}
+
+const test = function(req, res){
+  // let para = req.param('url') || '';
+  const itemId = req.param;
+  res.send(itemId);
+}
+app.get("/seller/data", getItemListFromDB); // add
+app.get("/seller/data/:itemId", test); // delete
+app.post("/seller/data", ); // modified
+app.get("/seller/data", ); // search
+
+// app.delete("/seller/data", getItemListFromDB);
+
+
+
+// ---------------------------- error handle ------------------------------
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
